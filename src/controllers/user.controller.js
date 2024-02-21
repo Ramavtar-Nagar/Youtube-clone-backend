80a677b9e31f8db88876ba01d227cb394e177943
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-02-22 03:33:06 +0530

    test and debugged all register controller using postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,7 +8,7 @@ const registerUser = asyncHandler( async (req, res) => {
     
     // get user details from frontend
     const { fullName, email, username, password } = req.body
-    console.log("Email : ", email);
+    // console.log("Email : ", email);
 
     // validation - not empty
     if (
@@ -28,7 +28,11 @@ const registerUser = asyncHandler( async (req, res) => {
 
     // check for images, check for avatar
     const avatarLocalPath =  req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
