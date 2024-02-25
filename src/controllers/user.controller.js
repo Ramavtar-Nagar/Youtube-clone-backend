3ed617aef1d629b227bc9735ef7a35866b54ce9f
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-02-25 23:36:24 +0530

    addad user avatar updating  controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -223,7 +223,7 @@ const refreshAccessToken = asyncHandler( async(req, res) => {
 const changeCurrentPassword = asyncHandler( async(req, res) => {
     const {oldPassword, newPassword} = req.body
 
-    const user = User.findById(req.user?.id)
+    const user = User.findById(req.user?._id)
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
 
     if (!isPasswordCorrect) {
@@ -244,11 +244,73 @@ const getCurrentUser = asyncHandler(async(req, res) => {
     .json(200, req.user, "Current user Fetched successfully")
 })
 
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    const {fullName, email} = req.body
+
+    if (!fullName || !email) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName: fullName,
+                email: email
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+})
+
+
+const updateUserAvtar = asyncHandler(async(req, res) => {
+    
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avtar file is Missing!")
+    }
+
+    const avatar = await uploadOnCloudinary(avtarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading your avtar")
+    }
+    
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar Image updated successfully")
+    )
+})
+
+
+
+
+
 export {
     registerUser,
     loginUser,
     logoutUser,
     refreshAccessToken,
     changeCurrentPassword,
-    getCurrentUser
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvtar,
+    updateUserCoverImage
 }
